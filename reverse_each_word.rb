8f41b65c7ed83066adacfc839a153be18b7550e4
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 19:24:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,6 @@
-def reverse_each_word(sentence)
-  reversed = []
-  
-end
 
-def reverse_word_order(sentence)
-reversed = []
-sentence.split.each do |word|
-  reversed.unshift word
-end
-reversed.join(' ')
-end
+
+
 
 
 
