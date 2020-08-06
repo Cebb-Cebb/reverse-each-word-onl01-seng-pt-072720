64d64c3d43054e19550304b0dbea1a0bce196b9d
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 19:16:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence1)
   new_array = []
   index = 0 
-  while index <sentence1.length
+  while index < sentence1.length
   new_array = sentence1{index} + new_array
+    index += 1
   end
   return new_array
 end
