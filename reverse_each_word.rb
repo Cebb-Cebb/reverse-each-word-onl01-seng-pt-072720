a37d83e5383c3fdb7c26eb391dc0c066fad9c09d
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 19:34:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,6 +14,8 @@ end
 #  end).join(' ')
 #end
 
+
+
   # returns that same sentence with each word reversed in place.
   #First solve it using .each Then write the same method using .collect to see the difference.
   #turn the string into an array
