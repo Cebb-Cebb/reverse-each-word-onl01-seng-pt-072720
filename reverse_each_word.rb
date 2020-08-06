f5cf01adc7fe79e37d751b1f33d2924134513cf1
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 19:28:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,9 @@
-def reverse_each_word(sentence)
-  reversed = (sentence.split.map do |word|
-    word.reverse
-  end).join(' ')
+def reverse_word_order(sentence)
+  reversed = []
+  sentence.split.each do |word|
+    reversed.unshift word
+  end
+  reversed.join(' ')
 end
 
-def reverse_each_word(sentence)
-  reversed = (sentence.split.collect do |word|
-    word.reversed
-  end).join(' ')
-end
 
