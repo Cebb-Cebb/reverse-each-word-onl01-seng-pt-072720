841888730b8afcc35b9c08fabfdefd3f9ad19c59
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 19:18:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,24 @@
-def reverse_each_word(sentence1)
-  new_array = []
+def reverse_each_word(sentence)
+  reversed = []
   
 end
 
+
+def reverse_each_word(sentence)
+  reversed = (sentence.split.map do |word|
+    word.reverse
+  end).join(' ')
+end
+
+puts reverse_each_word("This is a sentence to reverse the words of")
+
+# Misread the assignment and thought it was this
+def reverse_word_order(sentence)
+  reversed = []
+  sentence.split.each do |word|
+    reversed.unshift word
+  end
+  reversed.join(' ')
+end
+
+puts reverse_word_order("This is a sentence to reverse the word order of")
